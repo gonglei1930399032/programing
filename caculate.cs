76bb1db0ff93a4e2160ab149efeba9f36c22c9c2
gong <1930399032@qq.com>
2022-03-08 10:57:07 +0800

    caculate.cs is created .

--- caculate.cs
+++ caculate.cs
@@ -1,14 +1,8 @@
 public static class caculate
 {
-	public int add(int a, int b)
+	
+	public int sub(int a,int b)
 	{
-		return a+b;
-	}
-	public int max(int a,int b)
-	{
-		if(a>b)
-			return a;
-		else
-			return b;
+		return a-b;
 	}
 }
